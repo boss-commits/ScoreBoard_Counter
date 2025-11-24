fbd780f6f46084749badf48e3c23cf24c8c08fcc
VINOTH RAJ <vinothrajingenieur@gmail.com>
2025-11-24 09:42:33 +0530

    Commit inital files
    A game Score_board tracker

---
+++ index.js
@@ -0,0 +1,35 @@
+let count=document.getElementById("score1")
+
+let count1=0
+function one(){
+  count1+=1
+  count.textContent=count1
+}
+
+function two(){
+  count1+=2
+  count.textContent=count1
+}
+
+function three(){
+  count1+=3
+  count.textContent=count1
+}
+
+let gcount=document.getElementById("g-score")
+let gcount1=0
+
+function one1(){
+  gcount1+=1
+  gcount.textContent=gcount1
+}
+
+function two2(){
+  gcount1+=2
+  gcount.textContent=gcount1
+}
+
+function three3(){
+  gcount1+=3
+  gcount.textContent=gcount1
+}
