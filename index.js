002f4a41ad694139f6d67cf87fe997023107d1f4
VINOTH RAJ <vinothrajingenieur@gmail.com>
2025-11-24 10:13:39 +0530

    Updated team names

--- index.js
+++ index.js
@@ -11,25 +11,34 @@ function two(){
   count.textContent=count1
 }
 
-function three(){
-  count1+=3
+function four(){
+  count1+=4
+  count.textContent=count1
+}
+
+function six(){
+  count1+=6
   count.textContent=count1
 }
 
 let gcount=document.getElementById("g-score")
 let gcount1=0
 
-function one1(){
+function r_one(){
   gcount1+=1
   gcount.textContent=gcount1
 }
 
-function two2(){
+function r_two(){
   gcount1+=2
   gcount.textContent=gcount1
 }
 
-function three3(){
-  gcount1+=3
+function r_four(){
+  gcount1+=4
+  gcount.textContent=gcount1
+}
+function r_six(){
+  gcount1+=6
   gcount.textContent=gcount1
 }
